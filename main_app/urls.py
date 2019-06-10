9d86e70d5207ed601a09974cceabc64ee31f458b
Dominic Parducci <dominic.parducci@Dominics-MacBook-Pro.local>
2019-06-10 16:08:13 -0700

    updated details

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('beasts/', views.beasts_index, name='index')
+    path('beasts/', views.beasts_index, name='index'),
+    path('beasts/<int:beast_id>/', views.beasts_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,18 @@
-class Beast:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-beasts = [
-    Beast('Lolo', 'tabby', 'foul little demon', 3),
-    Beast('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Beast('Raven', 'black tripod', '3 legged cat', 4)
-]
-
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Beast
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1> Hi Hi Home </h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def beasts_index(request):
+    beasts = Beast.objects.all()
     return render(request, 'beasts/index.html', { 'beasts': beasts })
+
+def beasts_detail(request, beast_id):
+    beast = Beast.objects.get(id=beast_id)
+    return render(request, 'beasts/detail.html', { 'beast': beast} )
